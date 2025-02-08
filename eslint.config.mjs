64da0fff39ddcc6b14a831b04c48a76df6d86574
Kyle Fiegener <psiradish@gmail.com>
2025-02-07 19:37:12 -0800

    Simmer down ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    //...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({
+        extends: ['next'],
+        rules: {
+            "@typescript-eslint/semi": ["warn", "always"],
+            "@typescript-eslint/no-unused-expressions": "warn",
+        },
+    }),
 ];
 
 export default eslintConfig;
